0511d118cdaf3402843426af169e1b84bbaee095
Paky-Simeone <pasquale.simeone.46@gmail.com>
2024-02-07 18:12:31 +0100

    bonus

---
+++ src/components/AppJumbo.vue
@@ -0,0 +1,19 @@
+<script>
+export default{};
+</script>
+
+<template>
+    <div>
+        <h2>Questo è il mio Jumbotron</h2>
+        <p>Ciao a tutti, ci vediamo domani a lezione!</p>
+    </div>
+</template>
+
+<style>
+h2{
+    color: chocolate;
+}
+p{
+    color: cornflowerblue;
+}
+</style>
